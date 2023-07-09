46cfef7cc1ef97ba3f6486411c95f0bbef0cbbdc
MJ Richards <98212812+Johannes-Harald@users.noreply.github.com>
2023-07-09 14:34:09 +0300

    Add files via upload

---
+++ mj-map-app/index.js
@@ -0,0 +1,45 @@
+
+function initialize() {
+    var mapOptions = {
+
+        zoom: 10,
+
+        center: new google.maps.LatLng(40.7128, -74.0060),
+
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+
+        minZoom: 2
+    };
+
+
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+
+    var infoWindow = new google.maps.InfoWindow();
+
+
+    var marker = new google.maps.Marker({
+        // Cordinates for Iran, Zanjan
+        position: new google.maps.LatLng(36.6769, 48.4963),
+        // Attach the marker
+        map: map,
+        // Tooltip on hover
+        title: 'Iran, Zanjan'
+    });
+
+
+    marker.addListener('click', function () {
+        infoWindow.setContent(marker.title);
+        infoWindow.open(map, marker);
+    });
+
+
+    google.maps.event.addDomListener(window, "resize", function () {
+        map.setCenter(mapOptions.center);
+    });
+
+
+}
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
